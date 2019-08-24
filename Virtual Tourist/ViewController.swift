04676fa54cc8aa3aad473ebb66cf9f613c992d3f
AlekseyK <aleksey.kabishau@gmail.com>
2019-08-24 18:20:11 -0400

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Aleksey on 8/24/19.
+//  Copyright © 2019 Aleksey Kabishau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
