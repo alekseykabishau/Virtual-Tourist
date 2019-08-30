6660d73e5fc7a48a3b2d708c6f22e26b6435d950
AlekseyK <aleksey.kabishau@gmail.com>
2019-08-29 23:54:58 -0400

    feat: Set and connect IBOutlets

--- Virtual Tourist/Controller/DetailViewController.swift
+++ Virtual Tourist/Controller/DetailViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 import MapKit
 
 class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var newColletion: UIButton!
+    
+    private let itemsPerRow: CGFloat = 2
+    private let sectionInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0.0, right: 0.0)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = editButtonItem
 
     }
 }
