f0f4ad48da951bdc0ef1baa5c0e69b7f6e02dc8d
AlekseyK <aleksey.kabishau@gmail.com>
2019-08-31 00:11:36 -0400

    feat: Set two column collection view flow layout

--- Virtual Tourist/Controller/DetailViewController.swift
+++ Virtual Tourist/Controller/DetailViewController.swift
@@ -80,3 +80,28 @@ extension DetailViewController: UICollectionViewDataSource {
         return cell
     }
 }
+
+extension DetailViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = (view.frame.size.width) - paddingSpace
+        let itemWidth = availableWidth / itemsPerRow
+        return CGSize(width: itemWidth, height: itemWidth)
+    }
+    
+    
+    /* don't need because working with only one section
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    */
+ 
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
+}
